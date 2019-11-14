cd51789858c42872842976f3ad6a9b8cdee532aa
qunahiu <quanhui812@gmail.com>
2019-11-14 23:13:52 +0700

    Final (i think so) for weather app

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
-
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 var ErrorModal = React.createClass({
     getDefaultProps: function () {
         return {
@@ -7,22 +8,33 @@ var ErrorModal = React.createClass({
         };
     },
     componentDidMount: function () {
-        var modal = new Foundation.Reveal($('#error-modal'));
-        modal.open();
-    },
-    propTypes: {
-        title: React.PropTypes.string,
-        message: React.PropTypes.string.isRequired
-    },
-    render: function () {
         var { title, message } = this.props;
-        return (
+
+        var modalMarkup = (
             <div id="error-modal" className="reveal tiny text-center" data-reveal="">
                 <h4>{title}</h4>
                 <p>{message}!</p>
                 <p>
                     <button className="button hollow" data-close="">Okay</button>
                 </p>
+            </div>
+        );
+
+        var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+        $(ReactDOM.findDOMNode(this)).html($modal);
+
+        var modal = new Foundation.Reveal($('#error-modal'));
+        modal.open();
+
+    },
+    propTypes: {
+        title: React.PropTypes.string,
+        message: React.PropTypes.string.isRequired
+    },
+    render: function () {
+        return (
+            <div>
+
             </div>
         );
     }
